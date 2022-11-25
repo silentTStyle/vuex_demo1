1c56db1704c935eac436cc0aeb3d03e800f0bd8e
silentTStyles <11231306+silenttstyles@user.noreply.gitee.com>
2022-11-25 15:36:21 +0800

    初始化计数器项目的基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <my-addition></my-addition>
+  <p>-----------------------------------</p>
+  <my-subtraction></my-subtraction>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Addition from './components/MyAdditon.vue'
+import Subtraction from './components/MySubtraction.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less" scoped></style>
---
+++ src/components/MySubtraction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>当前最新的 count 值是：</h3>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style></style>
